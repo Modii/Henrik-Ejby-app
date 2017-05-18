c9db238e2beb667881dd62a1a1cba2da75e21855
Morten Henningsen <modii3@gmail.com>
2017-05-18 10:52:46 +0200

    fixed errors

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,17 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
-module.exports.metadata = 
+module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-console": "1.0.7",
     "cordova-plugin-whitelist": "1.3.2",
-<<<<<<< HEAD
+
     "cordova-plugin-compat": "1.0.0",
     "cordova-plugin-geolocation": "2.4.3"
-=======
+
     "cordova-plugin-geolocation": "2.4.3",
     "cordova-plugin-compat": "1.0.0"
->>>>>>> cb175a55366bf700b68a3b3522254728fe0284ca
+
 }
 // BOTTOM OF METADATA
 });
