d3e58831b5db6a278d57a873b7106847543bba4a
modii <modii3@gmail.com>
2017-05-22 15:01:44 +0200

    fixet back button

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -26,7 +26,7 @@ function initMap() {
 }
 */
 var map, infoWindow;
-      function initMap() {
+      function initMap(jQuery) {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 55.001, lng: 11.981},
         });
@@ -71,6 +71,15 @@ myApp.onPageInit('about', function (page) {
     });
 });
 
+
+myApp.onPageInit('index', function (page) {
+  var mapOptions = {
+    zoom: 12,
+  };
+  map = new google.maps.Map($$(page.container).find('#map')[0], mapOptions);
+  initMap();
+})
+
 // Generate dynamic page
 var dynamicPageIndex = 0;
 function createContentPage() {
@@ -99,3 +108,5 @@ function createContentPage() {
     );
 	return;
 }
+
+/* JQuery below */
