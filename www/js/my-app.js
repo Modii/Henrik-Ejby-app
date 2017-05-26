89eb9fa6d5fd55c6d1cdfc9e6bf0252e8a85fa6b
modii <modii3@gmail.com>
2017-05-26 17:23:40 +0200

    added custom markers to map + geolocation fix

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -45,12 +45,10 @@ var map, infoWindow;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           disableDefaultUI: true,
-          center: {lat: 55.001, lng: 11.981},
           zoom: 12,
           draggable: true,
           mapTypeControlOptions: {
           mapTypeIds: ['hybrid']
-
       }
         });
         map.mapTypes.set('hybrid');
@@ -66,9 +64,54 @@ var map, infoWindow;
             };
 
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Her er du.');
-            infoWindow.open(map);
+            var iconBase = 'img/';
+            var icons = {
+              ejbybolig: {
+                icon: iconBase + 'boligmarker.png'
+              },
+              library: {
+                icon: iconBase + 'hermarker.png'
+              }
+
+            };
+            var features = [
+              {
+                position: new google.maps.LatLng(pos),
+                type: 'library'
+              }, {
+                position: new google.maps.LatLng(55.006765, 11.958317),
+                type: 'ejbybolig'
+              }, {
+                position: new google.maps.LatLng(55.002286, 11.981101),
+                type: 'ejbybolig'
+              }, {
+                position: new google.maps.LatLng(55.001000, 11.961219),
+                type: 'ejbybolig'
+              }, {
+                position: new google.maps.LatLng(55.001951, 11.956188),
+                type: 'ejbybolig'
+              }, {
+                position: new google.maps.LatLng(55.006123, 11.969393),
+                type: 'ejbybolig'
+              }, {
+                position: new google.maps.LatLng(54.759255, 11.876671),
+                type: 'ejbybolig'
+              }, {
+                position: new google.maps.LatLng(54.768241, 11.869110),
+                type: 'ejbybolig'
+              }
+            ];
+                  // Create markers.
+                  features.forEach(function(feature) {
+                    var marker = new google.maps.Marker({
+                      position: feature.position,
+                      icon: icons[feature.type].icon,
+                      map: map,
+                      animation:google.maps.Animation.DROP
+                    });
+                  });
             map.setCenter(pos);
+
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
@@ -76,16 +119,18 @@ var map, infoWindow;
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
-      }
+
+
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
                               'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
+                              'Error: Your phone doesn\'t support geolocation.');
         infoWindow.open(map);
       }
 
+}
 
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
