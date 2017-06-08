19c0d6a5edcdd0f1ea0a9f6e3bbf93337169f45e
modii <modii3@gmail.com>
2017-06-08 19:57:52 +0200

    compiled/builded til apk fil

--- platforms/android/assets/www/js/my-app.js
+++ platforms/android/assets/www/js/my-app.js
@@ -22,23 +22,46 @@ $$('.panel-close').on('click', function () {
 });
 
 
-/*
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 55.001, lng: 11.981},
-    zoom: 8
-  });
-  var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(55.001383, 11.981929),
-      map: map,
-      title: "This is a marker!",
-      animation: google.maps.Animation.DROP
-  });
-}
-*/
+google.maps.event.addDomListener(window,'load',initMap);
+
 var map, infoWindow;
       function initMap() {
+
+        /* datasæt til markers herunder */
+        var locations = [
+           ['<div class="test">ejbybolig1</div>',55.006765, 11.958317, 1],
+           ['<div class="test">ejbybolig2</div>',55.002286, 11.981101, 1],
+           ['<div class="test">ejbybolig3</div>',55.001000, 11.961219, 1],
+           ['<div class="test">ejbybolig4</div>',55.001951, 11.956188, 1],
+           ['<div class="merebolig">' +
+           '<img class="merebolig-img" src="img/boliger/naverparken13.png">' +
+           '<p class="merebolig-tekst"> Naverparken 13, Nyråd <br>'+
+           '4760 Vordingborg <br>'+
+           'Kontantpris: 1.295.000 <br>'+
+           'Ejer udgifter pr. md.: 2355,72 <br>'+
+           'Boligareal: 142 <br></p>' +
+           '<a href="#" class="laesmereknap">LÆS MERE</a></div>',55.006123, 11.969393, 1],
+
+           ['<div class="merebolig">' +
+           '<img class="merebolig-img" src="img/boliger/gedservej8.jpg">' +
+           '<p class="merebolig-tekst"> Gedservej 8, Nykøbing F <br>'+
+           '4800 Nykøbing F <br>'+
+           'Kontantpris: 750.000 <br>'+
+           'Ejer udgifter pr. md.: 1322,14 <br>'+
+           'Boligareal: 71 <br></p>' +
+           '<a href="#" class="laesmereknap">LÆS MERE</a></div>',54.759255, 11.876671, 1],
+
+           ['<div class="merebolig">' +
+           '<img class="merebolig-img" src="img/boliger/kongensgade6.jpg">' +
+           '<p class="merebolig-tekst"> Kongensgade 6, Nykøbing F <br>'+
+           '4800 Nykøbing F <br>'+
+           'Kontantpris: 3.995.000 <br>'+
+           'Ejer udgifter pr. md.: 2670,41 <br>'+
+           'Boligareal: 298 <br></p>' +
+           '<a href="#" class="laesmereknap">LÆS MERE</a></div>',54.768241, 11.869110, 1],
+        ];
+
+        /* her oprettes vores kort */
         map = new google.maps.Map(document.getElementById('map'), {
           disableDefaultUI: true,
           zoom: 12,
@@ -49,9 +72,69 @@ var map, infoWindow;
         });
         map.mapTypes.set('hybrid');
         map.setMapTypeId('hybrid');
-        infoWindow = new google.maps.InfoWindow;
+        var infoWindow = new google.maps.InfoWindow;
 
-        // Try HTML5 geolocation.
+
+        /* infobubble er vores læs mere vinduer på kortet, herunder customizes de */
+        infoBubble = new InfoBubble({
+          map: map,
+          maxWidth: 250,
+          maxHeight: 260,
+          shadowStyle: 0,
+          padding: 5,
+          backgroundColor: '#fff',
+          borderRadius: 0,
+          arrowSize: 12,
+          borderWidth: 1,
+          borderColor: '#fff',
+          disableAutoPan: true,
+          hideCloseButton: false,
+          closeSrc: 'img/infobubbleclosebutton.png',
+          arrowPosition: 30,
+          backgroundClassName: 'boligmarkerbackground',
+          arrowStyle: 0
+        });
+
+        var marker, i;
+
+        /* Dette for loop opretter bolig markers */
+        for (i = 0; i < locations.length; i++) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map,
+            icon: 'img/boligmarker.png',
+            animation:google.maps.Animation.DROP
+          });
+
+          /* følgende loop gør så infovinduet lukkes ved klik på map */
+          google.maps.event.addListener(marker, 'click', function() {
+                     if(!marker.open){
+                         infoBubble.open(map,marker);
+                         marker.open = true;
+                     }
+                     else{
+                         infoBubble.close();
+                         marker.open = false;
+                     }
+                     google.maps.event.addListener(map, 'click', function() {
+                         infoBubble.close();
+                         marker.open = false;
+                     });
+         });
+          /* denne funktion venter på klik og åbner et info vindue til boligerne */
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infoBubble.close();
+              infoBubble.setContent(locations[i][0]);
+              map.setCenter(marker.getPosition());
+              infoBubble.open(map, marker);
+              /* centrerer læs mere boksen ved at rykke kortet 50px til venstre og 170px op */
+              map.panBy(50, -170);
+            }
+          })(marker, i));
+        }
+
+        // geolocation.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
@@ -62,39 +145,14 @@ var map, infoWindow;
             infoWindow.setPosition(pos);
             var iconBase = 'img/';
             var icons = {
-              ejbybolig: {
-                icon: iconBase + 'boligmarker.png'
-              },
-              library: {
+              hermarker: {
                 icon: iconBase + 'hermarker.png'
               }
-
             };
             var features = [
               {
                 position: new google.maps.LatLng(pos),
-                type: 'library'
-              }, {
-                position: new google.maps.LatLng(55.006765, 11.958317),
-                type: 'ejbybolig'
-              }, {
-                position: new google.maps.LatLng(55.002286, 11.981101),
-                type: 'ejbybolig'
-              }, {
-                position: new google.maps.LatLng(55.001000, 11.961219),
-                type: 'ejbybolig'
-              }, {
-                position: new google.maps.LatLng(55.001951, 11.956188),
-                type: 'ejbybolig'
-              }, {
-                position: new google.maps.LatLng(55.006123, 11.969393),
-                type: 'ejbybolig'
-              }, {
-                position: new google.maps.LatLng(54.759255, 11.876671),
-                type: 'ejbybolig'
-              }, {
-                position: new google.maps.LatLng(54.768241, 11.869110),
-                type: 'ejbybolig'
+                type: 'hermarker'
               }
             ];
                   // Create markers.
